9bfbfb68c0a3a617079e7b8aa11e0070d0d7d1ef
Cyril Dumange <cyril.dumange@adevinta.com>
2023-02-18 08:39:06 +0100

    chore: initial commit
    - add an either struct to manage cleanly error return
    - add fizzbuzz logic + test

---
+++ src/app/models/fizzbuzz.ts
@@ -0,0 +1,12 @@
+export type FizzbuzzRequest = {
+    int1 : number
+    int2 : number
+    str1 : string
+    str2 : string
+    limit : number
+}
+
+
+export type FizzbuzzResponse = {
+    result : string[]
+}
