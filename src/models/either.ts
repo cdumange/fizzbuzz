45181cf24441d75c0e77275afaa1f0f0fdc03d0a
Cyril Dumange <cdumange@gmail.com>
2023-07-15 11:44:26 -0400

    add prettier

--- src/models/either.ts
+++ src/models/either.ts
@@ -1,45 +1,45 @@
 export type Value<T> = {
-    value : T
-    error? : never
-}
-export function makeValue<T>(value : T) : Value<T> {
-    return {
-        value: value
-    }
+  value: T;
+  error?: never;
+};
+export function makeValue<T>(value: T): Value<T> {
+  return {
+    value: value,
+  };
 }
 
 export type Error<K> = {
-    value? : never
-    error : K
-}
-export function makeError<K>(error : K) : Error<K> {
-    return {
-        error: error
-    }
-}
-
-export type Either<T, U> = NonNullable<Value<T> | Error<U>>
-
-export function isError<T, K>(either : Either<T,K>) : boolean {
-    return either.error != undefined
+  value?: never;
+  error: K;
+};
+export function makeError<K>(error: K): Error<K> {
+  return {
+    error: error,
+  };
 }
 
-export function isValue<T, K>(either : Either<T,K>) : boolean {
-    return either.value != undefined
+export type Either<T, U> = NonNullable<Value<T> | Error<U>>;
+
+export function isError<T, K>(either: Either<T, K>): boolean {
+  return either.error != undefined;
 }
 
-export function eitherValue<T, K>(either: Either<T, K>) : T|K {
-    if (either.value == null && either.error == null) {
-        throw new Error("received an empty either")
-    }
+export function isValue<T, K>(either: Either<T, K>): boolean {
+  return either.value != undefined;
+}
 
-    if (isError(either)) {
-        return either.error as NonNullable<K>
-    }
+export function eitherValue<T, K>(either: Either<T, K>): T | K {
+  if (either.value == null && either.error == null) {
+    throw new Error("received an empty either");
+  }
 
-    if (isValue(either)) {
-        return either.value as NonNullable<T>
-    }
+  if (isError(either)) {
+    return either.error as NonNullable<K>;
+  }
 
-    throw new Error("something weird happened with " + JSON.stringify(either))
+  if (isValue(either)) {
+    return either.value as NonNullable<T>;
+  }
+
+  throw new Error("something weird happened with " + JSON.stringify(either));
 }
