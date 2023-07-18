fba6fd968f2d16f6dea99c6e409a0f194043343b
Cyril Dumange <cdumange@gmail.com>
2023-07-18 10:29:40 -0400

    prettier rules

--- src/routing/fizzbuzz.ts
+++ src/routing/fizzbuzz.ts
@@ -1,31 +1,37 @@
-import * as Koa from 'koa';
-import * as Router from 'koa-router';
+import * as Koa from "koa";
+import * as Router from "koa-router";
 
-import {Either, isError} from "../models/either"
-import { FizzbuzzRequest, FizzbuzzResponse, FizzBuzzErrors} from "../models/fizzbuzz"
+import { Either, isError } from "../models/either";
+import {
+  FizzbuzzRequest,
+  FizzbuzzResponse,
+  FizzBuzzErrors,
+} from "../models/fizzbuzz";
 
-import * as HttpStatus from 'http-status-codes';
+import * as HttpStatus from "http-status-codes";
 
-type functionFizzbuzz = (input: FizzbuzzRequest) => Either<FizzbuzzResponse, FizzBuzzErrors>
+type functionFizzbuzz = (
+  input: FizzbuzzRequest,
+) => Either<FizzbuzzResponse, FizzBuzzErrors>;
 
-export function registerFizzbuzz(api : Koa, fizzbuzz: functionFizzbuzz) {
-    const router: Router = new Router({
-        prefix: '/fizzbuzz',
-    });
+export function registerFizzbuzz(api: Koa, fizzbuzz: functionFizzbuzz) {
+  const router: Router = new Router({
+    prefix: "/fizzbuzz",
+  });
 
-    router.post('/', async (ctx: Koa.Context) : Promise<void> => {
-        const request = ctx.request.body as FizzbuzzRequest
-        const resp = fizzbuzz(request)
+  router.post("/", async (ctx: Koa.Context): Promise<void> => {
+    const request = ctx.request.body as FizzbuzzRequest;
+    const resp = fizzbuzz(request);
 
-        if (isError(resp)) {
-            ctx.response.status = HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR
-            return
-        }
+    if (isError(resp)) {
+      ctx.response.status = HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR;
+      return;
+    }
 
-        ctx.response.status = HttpStatus.StatusCodes.OK
-        ctx.response.body = resp.value
-    })
+    ctx.response.status = HttpStatus.StatusCodes.OK;
+    ctx.response.body = resp.value;
+  });
 
-    api.use(router.routes())
-    api.use(router.allowedMethods())
+  api.use(router.routes());
+  api.use(router.allowedMethods());
 }
